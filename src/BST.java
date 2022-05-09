54ad9a6db6614449050863581de1c2de4511f758
asl <theruslanov@mail.ru>
2022-05-09 23:14:21 +0600

    Asik3assik

--- src/BST.java
+++ src/BST.java
@@ -2,7 +2,7 @@
         import java.util.Iterator;
         import java.util.Spliterator;
         import java.util.function.Consumer;
-public class BST <K extends Comparable<K>, V>{
+public class BST <K extends Comparable<K>, V> {
     private Node root;
 
     private class Node {
@@ -17,11 +17,11 @@ public class BST <K extends Comparable<K>, V>{
         }
     }
 
-    public BST(){
+    public BST() {
         root = null;
     }
 
-    public BST(K key, V val){
+    public BST(K key, V val) {
         root = new Node(key, val);
     }
 
@@ -29,56 +29,127 @@ public class BST <K extends Comparable<K>, V>{
         root = insert(root, key, val);
     }
 
-    private Node insert(Node root, K key, V val){
-        if (root == null){
+    private Node insert(Node root, K key, V val) {
+        if (root == null) {
             root = new Node(key, val);
             return root;
         }
-        if (key.compareTo(root.key) < 0){
+        if (key.compareTo(root.key) < 0) {
             root.left = insert(root.left, key, val);
-        }
-        else{
+        } else {
             root.right = insert(root.right, key, val);
         }
         return root;
     }
 
-    public void inOrder(){
+    public void inOrder() {
         inOrder(root);
     }
 
-    public void postOrder(){
+    public void postOrder() {
         postOrder(root);
     }
 
-    private void postOrder(Node root){
-        if (root != null){
+    private void postOrder(Node root) {
+        if (root != null) {
             postOrder(root.right);
             System.out.println(root.key + " -> " + root.val);
             postOrder(root.left);
         }
     }
-    private void inOrder(Node root){
-        if (root != null){
+
+    private void inOrder(Node root) {
+        if (root != null) {
             inOrder(root.left);
             System.out.println(root.key + " -> " + root.val);
             inOrder(root.right);
         }
     }
 
-    public V get(K key){
+    public V get(K key) {
         return get(root, key);
     }
 
-    private V get(Node root, K key){
-        if (root == null){
+    private V get(Node root, K key) {
+        if (root == null) {
             return null;
         }
-        if (root.key.compareTo(key) == 0){
+        if (root.key.compareTo(key) == 0) {
             return root.val;
         }
         if (root.key.compareTo(key) > 0) return get(root.left, key);
         return get(root.right, key);
     }
+
+    public void delete(K key) {
+        delete(root, key);
+    }
+
+    private Node delete(Node root, K key) {
+        if (root == null) return root;
+
+        if (key.compareTo(root.key) < 0) {
+            root.left = delete(root.left, key);
+        } else if (key.compareTo(root.key) > 0) {
+            root.right = delete(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            root.key = minNode(root.right);
+            root.right = delete(root.right, root.key);
+        }
+        return root;
+    }
+
+    private K minNode(Node root) {
+        K mn = root.key;
+        while (root.left != null) {
+            mn = root.left.key;
+            root = root.left;
+        }
+        return mn;
+    }
+
+    public K secondMin() {
+        return secondMin(root);
+    }
+
+
+    public K secondMin(Node root) {
+        //TODO
+        return null;
+    }
+
+    public Iterable<K> iterator() {
+        //TODO
+        return null;
+    }
+
+    private class BSTIterator implements Iterator<K> {
+
+        public BSTIterator() {
+
+        }
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public K next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
+    }
+}
+
 
 
